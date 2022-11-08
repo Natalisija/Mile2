736b8ee540ebf8163cc509c29e99aedc41404188
Natalia Nemec <natalisija@bk.ru>
2022-11-08 15:47:34 +0300

    add Mile2

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 2000; // стоимость билета
+        int bonus = cost / 20; // количество бонусных миль
+
+        System.out.println("Количество бонусных миль с одного билета:");
+        System.out.println((cost / 20) + " миль ");
+    }
+}
